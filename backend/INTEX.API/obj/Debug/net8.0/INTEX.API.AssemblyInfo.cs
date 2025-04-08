a182fe6122c868988b50bf8a9ab8516442061169
Riley Wells <rwells9@byu.edu>
2025-04-08 08:26:48 -0600

    Adding CSS and changing the MoviesDetailPage

--- backend/INTEX.API/obj/Debug/net8.0/INTEX.API.AssemblyInfo.cs
+++ backend/INTEX.API/obj/Debug/net8.0/INTEX.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INTEX.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b365deb8a61ffdc721440840000d6f1650c4ea4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b4aee3a750cf0b3c6fd27d651c46bf5a79c918")]
 [assembly: System.Reflection.AssemblyProductAttribute("INTEX.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INTEX.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
