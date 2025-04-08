c1e78cbed656631fb69c2d8979f9398a12f1f461
valurisho <153689011+valurisho@users.noreply.github.com>
2025-04-08 14:32:57 -0600

    Landing Page!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminMoviePage from './pages/AdminMoviePage';
-import PrivacyPolicy from './pages/PrivacyPolicy';
+import HomePage from './pages/HomePage'; // Add this import
+import PrivacyPolicy from './pages/PrivacyPolicy'; // Adjust the path if necessary
 import MovieDetailPage from './pages/MovieDetailPage';
 import AddMoviePage from './pages/AddMoviePage';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import EditMoviePage from './pages/EditMoviePage';
 
-
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/" element={<HomePage />} />
           <Route path="/AdminPage" element={<AdminMoviePage />} />
           <Route path="/mainPage" element={<MainPage />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
